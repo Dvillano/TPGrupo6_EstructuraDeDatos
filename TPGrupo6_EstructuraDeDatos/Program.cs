e21d619b835706bf64ac1603be8e1ee3d82a4e95
Dvillano <danie.villano@gmail.com>
2021-11-11 15:30:29 -0300

    Primer commit

--- TPGrupo6_EstructuraDeDatos/Program.cs
+++ TPGrupo6_EstructuraDeDatos/Program.cs
@@ -4,62 +4,36 @@ using System.Collections;
 
 namespace TPGrupo6_EstructuraDeDatos
 {
-
-    //Clase candidato
-    class Candidato
-    {
-        public Candidato(string nombre1, string apellido1, string partido_politico1)
-        {
-            nombre = nombre1;
-            apellido = apellido1;
-            partido_politico = partido_politico1;
-        }
-
-        public string nombre;
-        public string apellido;
-        public string partido_politico;
-    }
-
     class Program
     {
         //Funcion para preguntar y guardar candidato
-        public static Candidato GuardarCandidato()
+        public static string[] GuardarCandidatos()
         {
-            Console.WriteLine("Ingrese el nombre  del candidato: ");
-            string nombre = Console.ReadLine();
+            string[] candidatos = new string[3];
 
-            Console.WriteLine("Ingrese el apellido del candidato: ");
-            string apellido = Console.ReadLine();
-
-            Console.WriteLine("Ingrese el partido politico del candidato: ");
-            string partidoPolitico = Console.ReadLine();
-
-            Candidato candidato = new Candidato(nombre, apellido, partidoPolitico);
-
-            return candidato;
-        }
-
-        public static void MostrarCandidatos(Candidato[] candidatos)
-        {
-            Console.WriteLine("========Lista de candidatos========");
             for (int i = 0; i < candidatos.Length; i++)
             {
-                Console.WriteLine("Candidato #" + i +
-                                   "\nNombre: " + candidatos[i].nombre +
-                                   " Apellido: " + candidatos[i].apellido +
-                                   " Partido politico: " + candidatos[i].partido_politico);
+                Console.WriteLine("Ingrese el nombre  del candidato: ");
+                candidatos[i] = Console.ReadLine();
+            }
+
+            return candidatos;
+        }
+
+        public static void MostrarCandidatos(string[] candidatos)
+        {
+            Console.WriteLine("========Lista de candidatos========");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Candidato #{0}" +
+                                   "\nNombre: " + candidatos[i], i+1);
             }
         }
 
         static void Main(string[] args)
         {
             //Parte 1 - Guardar candidatos
-            Candidato[] candidatos = new Candidato[3];
-            for (int i = 0; i < 3; i++)
-            {
-                candidatos[i] = GuardarCandidato();
-            }
-
+            string[] candidatos = GuardarCandidatos();
             MostrarCandidatos(candidatos);
 
             //Parte 2 - Ingresar votos
