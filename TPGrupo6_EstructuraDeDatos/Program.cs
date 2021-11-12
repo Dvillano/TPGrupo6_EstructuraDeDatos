9feef11bc20cf1a657361190212f448866f05465
Dvillano <danie.villano@gmail.com>
2021-11-11 21:07:22 -0300

    Agregada parte 2 y 3

--- TPGrupo6_EstructuraDeDatos/Program.cs
+++ TPGrupo6_EstructuraDeDatos/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections;
-
+using System.Collections.Generic;
 
 namespace TPGrupo6_EstructuraDeDatos
 {
@@ -20,6 +19,7 @@ namespace TPGrupo6_EstructuraDeDatos
             return candidatos;
         }
 
+        //Funcion para mostrar candidatos
         public static void MostrarCandidatos(string[] candidatos)
         {
             Console.WriteLine("========Lista de candidatos========");
@@ -30,15 +30,52 @@ namespace TPGrupo6_EstructuraDeDatos
             }
         }
 
+        public static string[] ingresarVotantes(string[] candidatos)
+        {
+            string[] votantes = new string[6];
+            int edad, voto;
+            Console.WriteLine("\n\nArranca la votacion\n");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("\nIngrese su edad: ");
+                edad = Convert.ToInt32(Console.ReadLine());
+                while (edad < 18)
+                {
+                    Console.Write("Usted no puede votar\n\nIngrese su edad: ");
+                    edad = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine("\n¿Por quien votara?\n");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine((j + 1) + "_ " + candidatos[j]);
+                }
+                Console.Write("\nElija a su candidato: ");
+                voto = Convert.ToInt32(Console.ReadLine());
+                while (voto < 1 || voto > 3)
+                {
+                    Console.Write("Ese candidato no existe. Vuelva a elegir su candidato: ");
+                    voto = Convert.ToInt32(Console.ReadLine());
+                }
+                votantes[i] = candidatos[voto - 1];
+            }
+            return votantes;
+        }
+
         static void Main(string[] args)
         {
+            Queue<string> cola = new Queue<string>();
             //Parte 1 - Guardar candidatos
             string[] candidatos = GuardarCandidatos();
             MostrarCandidatos(candidatos);
 
             //Parte 2 - Ingresar votos
+            string[] votantes = ingresarVotantes(candidatos);
 
-            //Parte 3 - Guardarlos en una cola
+            //Parte 3 - Guardar votos en una cola
+            for (int i = 0; i < 6; i++)
+            {
+                cola.Enqueue(votantes[i]);
+            }
 
             //Parte 4 - Contar votos y guardar ganador
 
